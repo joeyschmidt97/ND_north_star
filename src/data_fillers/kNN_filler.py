139f424693bc3e214963a76e740ee0e8a1b35c16
joeyschmidt97 <joseph.schmidt@utexas.edu>
2024-05-18 16:10:29 -0500

    Got stuck on kNN filler with dataset compatible... happening in the data merge perhaps

--- src/data_fillers/kNN_filler.py
+++ src/data_fillers/kNN_filler.py
@@ -4,52 +4,44 @@ from sklearn.neighbors import KNeighborsRegressor
 from ND_north_star.src.data_fillers.find_empty_points import find_empty_data_points
 
 
-def kNN_data_filler(incomplete_dataset_dict:dict, k:int = 3, resolution_list:list = 'auto'):
+def kNN_data_filler(sparse_dataset_dict:dict, k:int = 3, resolution_list:list = 'auto'):
 
-    real_coord_list = incomplete_dataset_dict['coordinates_list']
-    real_values = incomplete_dataset_dict['values_array']
-
-    real_coord_array = np.array(real_coord_list).T # Convert from [x1,x2,x3,...] to [[x1_1,x2_1,x3_1,...], [x1_2,x2_2,x3_2,...], ...]
-
+    sparse_features = sparse_dataset_dict['features']
+    sparse_values = sparse_dataset_dict['values']
+    sparse_resolution = sparse_dataset_dict['resolution']
 
     knn = KNeighborsRegressor(n_neighbors=k)
-    knn.fit(real_coord_array, real_values)
-
-
-    computed_dataset_dict = find_empty_data_points(incomplete_dataset_dict, resolution_list)
-    full_coord_list = computed_dataset_dict['coordinates_list']
-    full_values = computed_dataset_dict['values_array']
-
-    # Create a mask for NaN values in full_values
-    nan_mask = np.isnan(full_values)
-    non_nan_mask = ~nan_mask # Invert the mask to get a mask for non-NaN values
-
-    # Filter the coordinates based on the NaN mask
-    compute_coords = np.array(full_coord_list).T[non_nan_mask]
-    compute_values = knn.predict(compute_coords)
+    knn.fit(sparse_features, sparse_values)
 
 
 
-    combined_coords = np.array(full_coord_list).T
-    combined_values = np.zeros_like(full_values)
+    # Generate filled coordinate lists for each resolution
+    array_per_dim = [np.linspace(0, 1, res) for res in sparse_resolution]
 
-    for i, coord in enumerate(combined_coords):
-        coord_tuple = tuple(coord)
+    # Create a meshgrid for the coordinates in all dimensions
+    # Create a meshgrid for the coordinates in all dimensions
+    meshgrid = np.meshgrid(*array_per_dim, indexing='ij')
+    # Flatten the meshgrid arrays and combine them into a feature matrix
+    full_feature_points = np.vstack([grid.flatten() for grid in meshgrid]).T
 
-        if coord_tuple in map(tuple, real_coord_array):
-            index = list(map(tuple, real_coord_array)).index(coord_tuple)
-            combined_values[i] = real_values[index]
-        elif coord_tuple in map(tuple, compute_coords):
-            index = list(map(tuple, compute_coords)).index(coord_tuple)
-            combined_values[i] = compute_values[index]
+    # Find missing points by checking which full_feature_points are not in sparse_features
+    sparse_set = set(map(tuple, sparse_features))
+    missing_points = np.array([point for point in full_feature_points if tuple(point) not in sparse_set])
 
-    # Create the combined dataset dictionary
-    combined_dataset_dict = {
-        'coordinates_list': full_coord_list,
-        'values_array': combined_values
-    }
+    # Predict values for the missing points using kNN
+    if missing_points.size > 0:
+        missing_values = knn.predict(missing_points)
+    else:
+        missing_values = np.array([])
+
+    # Combine the sparse dataset with the newly computed missing points and values
+    combined_features = np.vstack((sparse_features, missing_points))
+    combined_values = np.concatenate((sparse_values, missing_values))
 
 
+    combined_dataset_dict = sparse_dataset_dict.copy()
+    combined_dataset_dict['features'] = combined_features
+    combined_dataset_dict['values'] = combined_values
 
     return combined_dataset_dict
     
