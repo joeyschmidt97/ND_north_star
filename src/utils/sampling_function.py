d3ac805e55bc1a9dfecaab0b8bbae30856526e92
joeyschmidt97 <joseph.schmidt@utexas.edu>
2024-05-18 15:27:13 -0500

    Updated random sampler and test code

--- src/utils/sampling_function.py
+++ src/utils/sampling_function.py
@@ -2,44 +2,21 @@ import numpy as np
 from scipy.spatial import cKDTree
 from scipy.stats import qmc
 
-def sample_coords_and_values(dataset_dict:dict, num_samples, method='random'):
+def random_sampler(dataset_dict:dict, num_samples, method='random'):
     """
-    Sample coordinates and values using different sampling methods.
 
-    Parameters:
-    coords (list): A list of arrays [x_arrays, y_arrays, ...] for each dimension of coordinates.
-    values (array): An array of values associated with the coordinates.
-    num_samples (int): The number of samples to be drawn.
-    method (str): The sampling method to be used ('random' or 'lhs').
-
-    Returns:
-    sampled_coords (list): A list of sampled coordinates [sampled_x, sampled_y, ...].
-    sampled_values (array): An array of values associated with the sampled coordinates.
     """
-    coords = dataset_dict['coordinates_list']
-    values = dataset_dict['values_array']
+    features = np.array(dataset_dict['features'])
+    values = np.array(dataset_dict['values'])
     
-    num_dimensions = len(coords)
-    assert all(len(arr) == len(values) for arr in coords), "Length of all coordinate arrays and values must be the same"
-    
-    coords_array = np.array(coords).T  # Transpose to shape (num_points, num_dimensions)
+    random_indices = np.random.choice(len(features), size=num_samples, replace=False)
 
-    if method == 'random':
-        indices = np.random.choice(len(coords[0]), num_samples, replace=False)
-    elif method == 'lhs':
-        sampler = qmc.LatinHypercube(d=num_dimensions)
-        sample = sampler.random(n=num_samples)
-        bounds = np.array([[np.min(arr), np.max(arr)] for arr in coords])
-        sampled_coords = qmc.scale(sample, bounds[:, 0], bounds[:, 1])
-        
-        tree = cKDTree(coords_array)
-        _, indices = tree.query(sampled_coords)
-    else:
-        raise ValueError("Invalid sampling method. Choose 'random' or 'lhs'.")
+    # Sampled features and values
+    sampled_features = features[random_indices]
+    sampled_values = values[random_indices]
 
-    sampled_coords = [coords[i][indices] for i in range(num_dimensions)]
-    sampled_values = values[indices]
-
-    sampled_dataset_dict = {'coordinates_list': sampled_coords, 'values_array': sampled_values}
+    sampled_dataset_dict = dataset_dict.copy()
+    sampled_dataset_dict['features'] = sampled_features
+    sampled_dataset_dict['values'] = sampled_values
 
     return sampled_dataset_dict
